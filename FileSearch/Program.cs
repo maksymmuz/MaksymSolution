ca0266f21ffaf8b1390e49febeec7aa7be32c70a
Maksym Muzyka <maksym_muzyka@epam.com>
2023-06-16 16:42:17 +0300

    Add project files.

---
+++ FileSearch/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+
+namespace FileSearch
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter the drive (example C:\\):");
+			string? searchPath = Console.ReadLine();
+
+			Console.WriteLine("Enter file name to search:");
+			string? fileName = Console.ReadLine();
+
+			try
+			{
+				string? filePath = SearchFile(searchPath, fileName);
+
+				if (!string.IsNullOrEmpty(filePath))
+				{
+					Console.WriteLine("File found: " + filePath);
+					ViewFileContents(filePath);
+					ArchiveFile(fileName);
+				}
+				else
+				{
+					Console.WriteLine("File not found.");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error occurred: " + ex.Message);
+			}
+		}
+
+		static string? SearchFile(string path, string fileName)
+		{
+			var directoriesToSearch = new Queue<string>();
+			directoriesToSearch.Enqueue(path);
+
+			while (directoriesToSearch.Count > 0)
+			{
+				string currentDirectory = directoriesToSearch.Dequeue();
+
+				string[] files = Directory.GetFiles(currentDirectory, fileName);
+				if (files.Length > 0)
+				{
+					return files[0];
+				}
+
+				string[] subDirectories = Directory.GetDirectories(currentDirectory);
+				foreach (string subDirectory in subDirectories)
+				{
+					directoriesToSearch.Enqueue(subDirectory);
+				}
+			}
+
+			return null;
+		}
+
+		static void ViewFileContents(string filePath)
+		{
+			using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+			{
+				using (StreamReader reader = new StreamReader(fileStream))
+				{
+					Console.WriteLine("File content:");
+					Console.WriteLine(new string('*', 20));
+					string? line;
+					while ((line = reader.ReadLine()) != null)
+					{
+						Console.WriteLine(line);
+					}
+					Console.WriteLine(new string('*', 20));
+				}
+			}
+
+			Process.Start("notepad.exe", filePath);
+		}
+
+		static void ArchiveFile(string fileName)
+		{
+			string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+			string sourceFilePath = Path.Combine(folderPath, fileName);
+			string destinationFilePath = Path.Combine(folderPath, "archive.zip");
+
+			FileStream source = File.OpenRead(sourceFilePath);
+			FileStream destination = File.Create(destinationFilePath);
+
+			GZipStream compressor = new GZipStream(destination, CompressionMode.Compress);
+
+			int theByte = source.ReadByte();
+			while (theByte != -1)
+			{
+				compressor.WriteByte((byte)theByte);
+				theByte = source.ReadByte();
+			}
+
+			// Видалення компресора.
+			compressor.Close();
+		}
+	}
+}
---
+++ SystemCollections/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Specialized;
+
+namespace SystemCollections
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Task2
+			Buyer buyer1 = new() { Name = "buyer1" };
+			Buyer buyer2 = new() { Name = "buyer2" };
+			Buyer buyer3 = new() { Name = "buyer3" };
+
+			List<string>? buyerCategory1 = new List<string>()
+			{
+				"Technic",
+			};
+
+			List<string>? buyerCategory2 = new List<string>()
+			{
+				"Food",
+				"Books",
+			};
+
+			List<string>? buyerCategory3 = new List<string>()
+			{
+				"Food",
+				"Technic",
+				"Books",
+			};
+
+			var buyers = new Dictionary<Buyer, List<string>>
+			{
+				{ buyer1, buyerCategory1 },
+				{ buyer2, buyerCategory2 },
+				{ buyer3, buyerCategory3 }
+			};
+
+			var categoryOfBuyer1 = buyers.Where(b => b.Key.Name == "buyer1");
+			var categoryOfBuyer2 = buyers.Where(b => b.Key.Name == "buyer2");
+			var categoryOfBuyer3 = buyers.Where(b => b.Key.Name == "buyer3");
+
+			var buyerByCategory = buyers.Where(c => !c.Value.Contains("Food"));
+			#endregion
+
+			#region Task3
+			Dictionary<int, decimal> counts1 = new()
+			{
+				{ 111, 10000.50m },
+				{ 222, 5000.25m },
+				{ 333, 7500.75m }
+			};
+
+			SortedDictionary<int, double> counts2 = new()
+			{
+				{ 111, 10000.50d },
+				{ 222, 5000.25d },
+				{ 333, 7500.75d }
+			};
+			#endregion
+		}
+	}
+}
