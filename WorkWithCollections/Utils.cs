25f8ee6064c8646e5ea8f1f8cb12cfb0283b619e
Maksym Muzyka <maksym_muzyka@epam.com>
2023-06-25 20:38:06 +0300

    added task 6 from lesson 1

---
+++ WorkWithCollections/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace WorkWithCollections
+{
+	internal static class Utils
+	{
+		public static IEnumerable SquaresOfOddNumbers(this IEnumerable array)
+		{
+			foreach (int item in array)
+			{
+				if (item % 2 != 0)
+				{
+					yield return Math.Pow(item, 2);
+				}
+			}
+		}
+	}
+}
