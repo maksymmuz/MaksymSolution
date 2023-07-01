e3838e471328b632e0272a0f845356cdde0c8c51
Maksym Muzyka <maksym_muzyka@epam.com>
2023-07-01 16:51:15 +0300

    added task 2 from lesson 4

---
+++ GettingInfoFromWebPage/Utilities.cs
@@ -0,0 +1,22 @@
+﻿namespace GettingInfoFromWebPage
+{
+	internal class Utilities
+	{
+		public static void WriteToFile(string filePath, string content, bool append = false)
+		{
+			try
+			{
+				using (var writer = new StreamWriter(filePath, append: append))
+				{
+					writer.WriteLine(content);
+				}
+
+				Console.WriteLine("The text was successfully written to the file.");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error occurred while writing to file: " + ex.Message);
+			}
+		}
+	}
+}
