c849c222ceb1bcf0a9c64b38670a5da9428b6c0b
Maksym Muzyka <maksym_muzyka@epam.com>
2023-07-02 00:05:48 +0300

    added task 3 from lesson 4 and Family tree project

---
+++ FamilyTree/FamilyTreeCollection.cs
@@ -0,0 +1,34 @@
+﻿namespace FamilyTree
+{
+	internal class FamilyTreeCollection
+	{
+		public FamilyTreeCollection(Person root)
+		{
+			Root = root;
+
+			LinkedList = new LinkedList<Person>();
+			LinkedList.AddFirst(root);
+		}
+
+		LinkedList<Person> LinkedList { get; set; }
+
+		public Person Root { get; set; }
+
+
+		//public void PrintFamilyTree()
+		//{
+		//	PrintFamilyTree(Root, 0);
+		//}
+
+		//private void PrintFamilyTree(Person person, int level)
+		//{
+		//	string indentation = new string(' ', level * 4);
+		//	Console.WriteLine($"{indentation}- {person}");
+
+		//	foreach (Person child in person.Children)
+		//	{
+		//		PrintFamilyTree(child, level + 1);
+		//	}
+		//}
+	}
+}
--- HelpLibrary/InputOutput.cs
+++ HelpLibrary/InputOutput.cs
@@ -1,6 +1,6 @@
-﻿namespace GettingInfoFromWebPage
+﻿namespace HelpLibrary
 {
-	internal class Utilities
+	public class InputOutput
 	{
 		public static void WriteToFile(string filePath, string content, bool append = false)
 		{
@@ -16,6 +16,19 @@
 			catch (Exception ex)
 			{
 				Console.WriteLine("Error occurred while writing to file: " + ex.Message);
+			}
+		}
+
+		public static string? ReadFromFile(string filePath)
+		{
+			try
+			{
+				return File.ReadAllText(filePath);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error occurred while reading the file: " + ex.Message);
+				return null;
 			}
 		}
 	}
