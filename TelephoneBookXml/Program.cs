1d5ac0bbff4d116d5c8b6045bc6de00811042686
Maksym Muzyka <maksym_muzyka@epam.com>
2023-07-03 01:10:26 +0300

    added task 2 from lesson 5

---
+++ TelephoneBookXml/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml;
+
+/**
+ * Домашнє завдання 5
+ * Завдання 2
+ * Створіть програму, яка виводить на екран всю інформацію про вказаний .xml файл.
+ */
+
+XmlReaderSettings settings = new XmlReaderSettings();
+settings.DtdProcessing = DtdProcessing.Ignore; // Disable XML compliance checking
+
+using (XmlReader reader = XmlReader.Create(@"TelephoneBook.xml", settings))
+{
+	while (reader.Read())
+	{
+		switch (reader.NodeType)
+		{
+			case XmlNodeType.Element:
+				Console.WriteLine($"Value: {reader.Name}");
+
+				if (reader.HasAttributes)
+				{
+					while (reader.MoveToNextAttribute())
+					{
+						Console.WriteLine($"Attribute: {reader.Name} = {reader.Value}");
+					}
+
+					reader.MoveToElement();
+				}
+				break;
+
+			case XmlNodeType.Text:
+				Console.WriteLine($"Value: {reader.Value}");
+				break;
+
+			case XmlNodeType.EndElement:
+				Console.WriteLine($"End of element: {reader.Name}");
+				break;
+		}
+	}
+}
