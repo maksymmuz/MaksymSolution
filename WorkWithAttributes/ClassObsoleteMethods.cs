024295678023d8955b97b02bcde2c4bbe69ba7ce
Maksym Muzyka <maksym_muzyka@epam.com>
2023-07-15 15:10:11 +0300

    added task 2 from lesson 7

---
+++ WorkWithAttributes/ClassObsoleteMethods.cs
@@ -0,0 +1,22 @@
+﻿namespace WorkWithAttributes
+{
+	internal class ClassObsoleteMethods
+	{
+		[Obsolete("My custom message: This method is obsolete.")]
+		public int ObsoleteSum(int a, int b) => a + b;
+
+
+		// in this case the code will not compiled due to second Obsolete attribute parameter = true
+		// [Obsolete("This method is obsolete. You can use the new one - Print()", true)]
+		[Obsolete("This method is obsolete. You can use the new one - Print()")]
+		public void ObsoletePrint() 
+		{
+			Console.WriteLine("Message from ObsoletePrint().");
+		}
+
+		public void Print()
+		{
+			Console.WriteLine("Message from Print().");
+		}
+	}
+}
---
+++ WorkWithAttributes/Program.cs
@@ -0,0 +1,14 @@
+﻿using WorkWithAttributes;
+
+/**
+ * Домашнє завдання 7
+ * Завдання 2
+ * Створіть клас і застосуйте до його методів атрибут Obsolete спочатку у формі, що просто виводить попередження,
+ * а потім у формі, що перешкоджає компіляції. Продемонструйте роботу атрибута з прикладу виклику даних методів.
+ * */
+
+ClassObsoleteMethods obsoleteMethods = new();
+
+Console.WriteLine(obsoleteMethods.ObsoleteSum(2, 5));
+obsoleteMethods.ObsoletePrint();
+obsoleteMethods.Print();
