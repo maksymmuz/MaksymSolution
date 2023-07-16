5e06bb726da0fc0437ff7ef0c97714b46006a56a
Maksym Muzyka <maksym_muzyka@epam.com>
2023-07-16 18:33:31 +0300

    added task 5 from lesson 7

---
+++ AccessLevel/Director.cs
@@ -0,0 +1,16 @@
+﻿namespace AccessLevel
+{
+	[AccessLevel(EmployeeAccessLevel.ReadOnly)]
+	internal class Director : Employee
+	{
+		public Director(string name)
+		{
+			Name = name;
+		}
+
+		public override void Work()
+		{
+			Console.WriteLine($"{Name} (Director) is making important decisions.");
+		}
+	}
+}
