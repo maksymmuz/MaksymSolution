7bf79a3eb2833d24663c82e09bc5d7e5f127b730
Maksym Muzyka <maksym_muzyka@epam.com>
2023-07-23 20:52:57 +0300

    added task 4 from lesson 9

---
+++ LargeObjectCleaningPattern/LargeObject.cs
@@ -0,0 +1,41 @@
+﻿namespace LargeObjectCleaningPattern
+{
+	internal class LargeObject : IDisposable
+	{
+		private int[] largeArray;
+		private bool disposed = false;
+
+		public LargeObject()
+		{
+			largeArray = new int[10_000_000];
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this); // Do not call the destructor.
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+					// additional resource release actions can be performed as needed
+
+					Console.WriteLine("The database connection is closed.");
+				}
+
+				// Release managed resources (array object)
+				largeArray = null;
+				disposed = true;
+			}
+		}
+
+		~LargeObject()
+		{
+			Dispose(false);
+		}
+	}
+}
