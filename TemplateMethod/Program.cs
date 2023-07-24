9054609957b746401bdc3d6f44b1183a8c066221
Maksym Muzyka <maksym_muzyka@epam.com>
2023-07-25 00:19:05 +0300

    added task 2 from lesson 10

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿using TemplateMethod;
+/**
+ * Домашнє завдання 10
+ * Завдання 2
+ * Вивчіть опис шаблону Template method (Шаблонний метод). Зверніть увагу на застосування шаблону,
+ * а також на склад його учасників і зв'язку відносини між ними. Напишіть невелику програму мовою C#, 
+ * що є абстрактною реалізацією даного шаблону.
+ */
+
+TemplateClass templateObject_1 = new ConcreteClass_1();
+TemplateClass templateObject_2 = new ConcreteClass_2();
+
+templateObject_1.LoadWebPageTemplateMethod();
+templateObject_2.LoadWebPageTemplateMethod();
---
+++ TemplateMethod/TemplateClass.cs
@@ -0,0 +1,18 @@
+﻿namespace TemplateMethod
+{
+	internal abstract class TemplateClass
+	{
+		public void LoadWebPageTemplateMethod()
+		{
+			LoadHeader();
+			LoadBody();
+			LoadFooter();
+		}
+
+		protected abstract void LoadHeader();
+
+		protected abstract void LoadBody();
+
+		protected abstract void LoadFooter();
+	}
+}
