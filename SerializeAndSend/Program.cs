346835f8f877f08e29620f28ea2d9041131527c3
Maksym Muzyka <maksym_muzyka@epam.com>
2023-07-26 00:04:02 +0300

    added task 5 from lesson 8

---
+++ SerializeAndSend/Program.cs
@@ -0,0 +1,26 @@
+﻿using SerializeAndSend;
+using System.IO.Compression;
+using System.Text;
+using System.Text.Json;
+/*
+ * Домашнє завдання 8
+ * Завдання 5 
+ * Створіть тип користувача (наприклад, клас) і виконайте серіалізацію об'єкта цього типу,
+ * враховуючи той факт, що стан об'єкта необхідно буде передати по мережі.
+ */
+
+var user = new User("Johnnie", 30);
+string jsonString = JsonSerializer.Serialize(user);
+
+byte[] compressedData;
+using (var memoryStream = new MemoryStream())
+{
+	using var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress);
+
+	byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+	gzipStream.Write(jsonBytes, 0, jsonBytes.Length);
+
+	compressedData = memoryStream.ToArray();
+}
+
+// compressedData for sending over the network
---
+++ SerializeAndSend/User.cs
@@ -0,0 +1,18 @@
+﻿namespace SerializeAndSend
+{
+	[Serializable]
+	public class User
+	{
+		public string Username { get; set; }
+
+		public int Age { get; set; }
+
+		public User() { }
+
+		public User(string username, int age)
+		{
+			Username = username;
+			Age = age;
+		}
+	}
+}
