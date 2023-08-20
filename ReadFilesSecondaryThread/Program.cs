83dc834646471583d7bf3d6c8108cdf68a198af5
Maksym Muzyka <maksym_muzyka@epam.com>
2023-08-20 17:27:28 +0300

    added 2, 3, 5 tasks from lesson 12

--- ReadFilesSecondaryThread/Program.cs
+++ ReadFilesSecondaryThread/Program.cs
@@ -4,48 +4,48 @@
  * Домашнє завдання 11
  * Завдання 2
  * Створіть консольну програму, яка в різних потоках зможе отримати доступ до 2-х файлів.
- * Зчитайте з цих файлів вміст і спробуйте записати отриману інформацію в третій файл. 
+ * Зчитайте з цих файлів вміст і спробуйте записати отриману інформацію в третій файл.
  * Читання/запис мають здійснюватися одночасно у кожному з дочірніх потоків.
  * Використовуйте блокування потоків для того, щоб досягти коректного запису в кінцевий файл.
  */
-
 internal class Program
 {
-    private static readonly object fileLock = new();
+	private static readonly object fileLock = new();
 
-    private static void Main()
-    {
-        Thread thread1 = new Thread(ReadAndWriteToFile);
-        Thread thread2 = new Thread(ReadAndWriteToFile);
-		
+	private static void Main()
+	{
+		Thread thread1 = new Thread(ReadAndWriteToFile);
+		Thread thread2 = new Thread(ReadAndWriteToFile);
+
 		thread1.Start("file1.txt");
 		thread2.Start("file2.txt");
-    }
+	}
 
-    private static void ReadAndWriteToFile(object filename)
-    {
-        string inputFileName = filename.ToString();
-        string outputFileName = "output.txt";
+	private static void ReadAndWriteToFile(object filename)
+	{
+		string inputFileName = filename.ToString();
+		string outputFileName = "output.txt";
 
-        try
-        {
-            string content;
-            lock (fileLock)
-            {
-                content = InputOutput.ReadFromFile(inputFileName);
-            }
+		try
+		{
+			string content;
+			lock (fileLock)
+			{
+				content = InputOutput.ReadFromFile(inputFileName);
+			}
 
-            Thread.Sleep(50);
-            lock (fileLock)
-            {
-                InputOutput.WriteToFile(outputFileName, content, true);
-            }
+			Thread.Sleep(50);
+			lock (fileLock)
+			{
+				InputOutput.WriteToFile(outputFileName, content, true);
+			}
 
-            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: read from {inputFileName} and write to {outputFileName}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error in the thread {Thread.CurrentThread.ManagedThreadId}: {ex.Message}");
-        }
-    }
+			Console.WriteLine(
+				$"Thread {Thread.CurrentThread.ManagedThreadId}: read from {inputFileName} and write to {outputFileName}");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error in the thread {Thread.CurrentThread.ManagedThreadId}: {ex.Message}");
+		}
+	}
 }
---
+++ SyncWithMonitorClass/Program.cs
@@ -0,0 +1,51 @@
+﻿using HelpLibrary;
+
+/**
+* Домашнє завдання 12
+* Завдання 2
+* Перетворіть приклад блокування подій таким чином,
+* щоб замість ручної обробки використовувалася автоматична.
+*/
+internal class Program
+{
+	private static readonly object lockObject = new();
+
+	public static void Main()
+	{
+		Thread thread1 = new Thread(ReadAndWriteToFile);
+		Thread thread2 = new Thread(ReadAndWriteToFile);
+
+		thread1.Start("file1.txt");
+		thread1.Join();
+
+		thread2.Start("file2.txt");
+		thread2.Join();
+	}
+
+	static void ReadAndWriteToFile(object fileNameObj)
+	{
+		string? fileName = fileNameObj.ToString();
+		string outputFileName = "output.txt";
+
+		string content;
+		Monitor.Enter(lockObject);
+		try
+		{
+			content = InputOutput.ReadFromFile(fileName);
+		}
+		finally
+		{
+			Monitor.Exit(lockObject);
+		}
+
+		Monitor.Enter(lockObject);
+		try
+		{
+			InputOutput.WriteToFile(outputFileName, content, true);
+		}
+		finally
+		{
+			Monitor.Exit(lockObject);
+		}
+	}
+}
